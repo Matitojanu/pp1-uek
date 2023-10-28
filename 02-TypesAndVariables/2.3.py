05af222e91eade4b30d936226aaf6d449ba7246d
Januszkiewicz, M. M. (Mateusz, Student B-BIT) <m.m.januszkiewicz@student.utwente.nl>
2023-10-28 13:22:52 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/2.3.py
@@ -0,0 +1,3 @@
+hours = int(input("Input hours: "))
+rate = int(input("Input rate: "))
+print("Pay:", hours*rate)
---
+++ 02-TypesAndVariables/2.5.py
@@ -0,0 +1,6 @@
+celsius = int(input("Enter Celsius temperature: "))
+
+fahrenheit = (celsius * 1.8) + 32
+ 
+# printing the result
+print("The temperature in Fahrenheit is:",fahrenheit)
